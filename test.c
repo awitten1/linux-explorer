9760564d384ba49adaf9cf7df18f9f9c58ab572d
awitten007@gmail.com <awitten1@terpmail.umd.edu>
2024-09-29 04:00:45 +0000

    mount proc into new namespace

--- test.c
+++ test.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <stdio.h>
+#include <sys/mount.h>
 
 int main() {
     if (unshare(CLONE_NEWPID) == -1) {
@@ -15,8 +16,17 @@ int main() {
     } else {
         pid_t my_pid = getpid();
         pid_t parent_pid = getppid();
+        if (unshare(CLONE_NEWNS) == -1) {
+            perror("unshare failed");
+            return 1;
+        }
+        // Remount /proc in the new mount namespace
+        if (mount("proc", "/proc", "proc", MS_PRIVATE, NULL) == -1) {
+            perror("mount failed");
+            return 1;
+        }
         printf("child pid %d, parent pid %d\n", my_pid, parent_pid);
-        sleep(60);
+        pause();
     }
     return 0;
 }
